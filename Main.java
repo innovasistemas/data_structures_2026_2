5831a6f17afca354334f16dc1ad40143f597b24e
unknown <innovasistemas@gmail.com>
2026-08-12 20:56:10 -0500

    Cadenas de caracteres (primer commit)

---
+++ Main.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Main 
+{
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        String option;
+        do {
+            System.out.println("Menú de opciones");
+            System.out.println("0. Salir");
+            System.out.println("1. Cadenas de caracteres");
+            System.out.println("2. Vectores");
+            System.out.println("3. Matrices");
+            System.out.print("Ingrese su opción: ");
+            option = input.next();
+            switch (option) {
+                case "0":
+                    System.out.println("Hasta pronto");
+                    break;
+                case "1":
+                    menuStrings();
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        } while (!option.equals("0"));
+    }
+
+    public static void menuStrings()
+    {
+        String option;
+        do {
+            System.out.println("Menú cadenas de caracteres");
+            System.out.println("0. Regresar");
+            System.out.println("1. Pruebas de métodos de cadena");
+            System.out.println("2. Palíndromo");
+            System.out.print("Ingrese su opción: ");
+            option = input.next();
+            switch (option) {
+                case "0":
+                    break;
+                case "1":
+                    
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        } while (!option.equals("0"));
+    }
+}
---
+++ com/packages/string/Strings.java
@@ -0,0 +1,44 @@
+public class Strings
+{
+    private String text;
+
+    public Strings()
+    {
+        text = "";
+    }
+
+    public void testStrings()
+    {
+        System.out.println("Start small. Ship something.");System.out.println("Longitud hola: " + "hola".length());
+        System.out.println("Longitud hola: " + "hola".length());
+        System.out.println("Concatenar: " + "hola " + "mundo" + " cruel");
+        System.out.println("Concatenar: " + "hola ".concat("mundo"));
+        System.out.println("texto convertido a mayúsculas".toUpperCase());
+        System.out.println("TEXTO CONVERTIDO A MINÚSCULAS".toLowerCase());
+        String n1, n2;
+        n1 = "Pedro";
+        n2 = "pedro";
+        System.out.println(n1 == n2);
+        System.out.println(n1.equals(n2));
+        System.out.println(n1.compareTo(n2));       
+        System.out.println(n1.equalsIgnoreCase(n2));
+        int x = 10;
+        String xStr = String.valueOf(x);
+        x = x + 1;
+        System.out.println(xStr);
+        System.out.println(x);
+        xStr = xStr + "1";
+        System.out.println(xStr);
+        String tex = "hoy no es martes";
+        int pos = tex.indexOf("no", 0);
+        System.out.println("Posición: " + pos);
+        String word = " hola ";
+        System.out.println("Longitud word: " + word.length());
+        word = word.trim();
+        System.out.println("Longitud word: " + word.length());
+        char c = tex.charAt(4);
+        System.out.println("Carácter: " + c);
+        System.out.println("ASCII c: " + (int)c);
+        System.out.println("ASCII A: " + (int)'A');
+    }
+}
